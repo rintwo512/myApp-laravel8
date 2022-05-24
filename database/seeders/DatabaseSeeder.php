ffd63cb7e8ca98687324502f4b83c85a6f4f76a0
rintwo512 <ryntooh@gmail.com>
2022-05-24 19:50:08 +0800

    meperbaruhi db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,146 +33,146 @@ class DatabaseSeeder extends Seeder
             'is_active' => 1
         ]);
 
-        CctvMonitor1::create([
-            'lantai' => 'Lantai 1',
-            'wing' => 'Wing A',
-            'lokasi' => 'Lobby',
-            'merk' => 'Hikvision',
-            'type' => 'Analog',
-            'status' => 'Rusak',
-            'kerusakan' => 'Kamera rusak',
-            'resolusi' => '2MP',
-            'tgl_pemasangan' => '1 April, 2015',
-            'petugas_pemasangan' => 'Vendor',
-            'sandi_dvr' => 'telkom/pola Z',
-            'user_id' => 1
-        ]);
+        // CctvMonitor1::create([
+        //     'lantai' => 'Lantai 1',
+        //     'wing' => 'Wing A',
+        //     'lokasi' => 'Lobby',
+        //     'merk' => 'Hikvision',
+        //     'type' => 'Analog',
+        //     'status' => 'Rusak',
+        //     'kerusakan' => 'Kamera rusak',
+        //     'resolusi' => '2MP',
+        //     'tgl_pemasangan' => '1 April, 2015',
+        //     'petugas_pemasangan' => 'Vendor',
+        //     'sandi_dvr' => 'telkom/pola Z',
+        //     'user_id' => 1
+        // ]);
 
-        Chart::create([
-            'tahun' => '2020',
-            'bulan' => 'Januari',
-            'total' => '15'
-        ]);
+        // Chart::create([
+        //     'tahun' => '2020',
+        //     'bulan' => 'Januari',
+        //     'total' => '15'
+        // ]);
 
-        Chart::create([
-            'tahun' => '2020',
-            'bulan' => 'Februari',
-            'total' => '22'
-        ]);
-        Chart::create([
-            'tahun' => '2020',
-            'bulan' => 'Maret',
-            'total' => '4'
-        ]);
-        Chart::create([
-            'tahun' => '2020',
-            'bulan' => 'April',
-            'total' => '7'
-        ]);
-        Chart::create([
-            'tahun' => '2020',
-            'bulan' => 'Mei',
-            'total' => '12'
-        ]);
-        Chart::create([
-            'tahun' => '2020',
-            'bulan' => 'Juni',
-            'total' => '9'
-        ]);
-        Chart::create([
-            'tahun' => '2020',
-            'bulan' => 'Juli',
-            'total' => '3'
-        ]);
-        Chart::create([
-            'tahun' => '2020',
-            'bulan' => 'Agustus',
-            'total' => '22'
-        ]);
-        Chart::create([
-            'tahun' => '2020',
-            'bulan' => 'September',
-            'total' => '6'
-        ]);
-        Chart::create([
-            'tahun' => '2020',
-            'bulan' => 'Oktober',
-            'total' => '8'
-        ]);
-        Chart::create([
-            'tahun' => '2020',
-            'bulan' => 'November',
-            'total' => '25'
-        ]);
-        Chart::create([
-            'tahun' => '2020',
-            'bulan' => 'Desember',
-            'total' => '35'
-        ]);
-
-
+        // Chart::create([
+        //     'tahun' => '2020',
+        //     'bulan' => 'Februari',
+        //     'total' => '22'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2020',
+        //     'bulan' => 'Maret',
+        //     'total' => '4'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2020',
+        //     'bulan' => 'April',
+        //     'total' => '7'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2020',
+        //     'bulan' => 'Mei',
+        //     'total' => '12'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2020',
+        //     'bulan' => 'Juni',
+        //     'total' => '9'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2020',
+        //     'bulan' => 'Juli',
+        //     'total' => '3'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2020',
+        //     'bulan' => 'Agustus',
+        //     'total' => '22'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2020',
+        //     'bulan' => 'September',
+        //     'total' => '6'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2020',
+        //     'bulan' => 'Oktober',
+        //     'total' => '8'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2020',
+        //     'bulan' => 'November',
+        //     'total' => '25'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2020',
+        //     'bulan' => 'Desember',
+        //     'total' => '35'
+        // ]);
 
 
-        Chart::create([
-            'tahun' => '2021',
-            'bulan' => 'Januari',
-            'total' => '15'
-        ]);
 
-        Chart::create([
-            'tahun' => '2021',
-            'bulan' => 'Februari',
-            'total' => '22'
-        ]);
-        Chart::create([
-            'tahun' => '2021',
-            'bulan' => 'Maret',
-            'total' => '4'
-        ]);
-        Chart::create([
-            'tahun' => '2021',
-            'bulan' => 'April',
-            'total' => '9'
-        ]);
-        Chart::create([
-            'tahun' => '2021',
-            'bulan' => 'Mei',
-            'total' => '8'
-        ]);
-        Chart::create([
-            'tahun' => '2021',
-            'bulan' => 'Juni',
-            'total' => '9'
-        ]);
-        Chart::create([
-            'tahun' => '2021',
-            'bulan' => 'Juli',
-            'total' => '7'
-        ]);
-        Chart::create([
-            'tahun' => '2021',
-            'bulan' => 'Agustus',
-            'total' => '11'
-        ]);
-        Chart::create([
-            'tahun' => '2021',
-            'bulan' => 'September',
-            'total' => '6'
-        ]);
-        Chart::create([
-            'tahun' => '2021',
-            'bulan' => 'Oktober',
-            'total' => '8'
-        ]);
-        Chart::create([
-            'tahun' => '2021',
-            'bulan' => 'November',
-            'total' => '20'
-        ]);
-        Chart::create([
-            'tahun' => '2021',
-            'bulan' => 'Desember',
-            'total' => '23'
-        ]);
+
+        // Chart::create([
+        //     'tahun' => '2021',
+        //     'bulan' => 'Januari',
+        //     'total' => '15'
+        // ]);
+
+        // Chart::create([
+        //     'tahun' => '2021',
+        //     'bulan' => 'Februari',
+        //     'total' => '22'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2021',
+        //     'bulan' => 'Maret',
+        //     'total' => '4'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2021',
+        //     'bulan' => 'April',
+        //     'total' => '9'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2021',
+        //     'bulan' => 'Mei',
+        //     'total' => '8'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2021',
+        //     'bulan' => 'Juni',
+        //     'total' => '9'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2021',
+        //     'bulan' => 'Juli',
+        //     'total' => '7'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2021',
+        //     'bulan' => 'Agustus',
+        //     'total' => '11'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2021',
+        //     'bulan' => 'September',
+        //     'total' => '6'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2021',
+        //     'bulan' => 'Oktober',
+        //     'total' => '8'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2021',
+        //     'bulan' => 'November',
+        //     'total' => '20'
+        // ]);
+        // Chart::create([
+        //     'tahun' => '2021',
+        //     'bulan' => 'Desember',
+        //     'total' => '23'
+        // ]);
     }
 }
